1f1f0382618a47b23e64f89c360eb62ecfa9040b
Abanoub Abbady <103790245+El-Abbady@users.noreply.github.com>
2024-09-23 17:38:23 +0300

    Delete Pharmacy.Api directory
    Bono

--- Pharmacy.Api/Dto/ShoppingCartDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Dto
-{
-    public class ShoppingCartDto
-    {
-        public int UserId { get; set; }
-        public int ProductId {  get; set; }
-        public int Quantity { get; set; }
-    }
-}
